1128a5605f6dd54a4b0c90acfaa7d55fd6996715
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2023-02-22 22:32:22 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,27 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String num = br.readLine();
+		
+		// 기본 Array (basicArr)
 		String split[] = num.split("");
-		Integer arr[] = new Integer[num.length()];
-		
-		for (int i = 0; i < num.length(); i++) {
-			arr[i] = Integer.parseInt(split[i]);
+		int maxNum = 0;
+		int basicArr[] = new int[split.length];
+		for (int i = 0; i < basicArr.length; i++) {
+			basicArr[i] = Integer.parseInt(split[i]);
+			
+			if (maxNum < basicArr[i]) {
+				maxNum = basicArr[i];
+			}
 		}
-	
-		Arrays.sort(arr, Collections.reverseOrder());
 		
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]);
+		// Counting Array (countingArr)
+		int countingArr[] = new int[maxNum+1];
+		for (int i = 0; i < basicArr.length; i++) {
+			countingArr[basicArr[i]]++;
+		}
+		
+		// 누적합으로 변경
+		for (int i = 1; i < countingArr.length; i++) {
+			countingArr[i] = countingArr[i] + countingArr[i-1];
+		}
+		
+		// 결과값 Array (resultArr)
+		int resultArr[] = new int[basicArr.length];
+		
+		for (int i = basicArr.length-1; i >=0; i--) {
+			int value = basicArr[i];
+			countingArr[value]--;
+			resultArr[countingArr[value]] = value;
+		}
+		
+		for (int i = resultArr.length-1; i >=0; i--) {
+			System.out.print(resultArr[i]);
 		}
 	}
 }
