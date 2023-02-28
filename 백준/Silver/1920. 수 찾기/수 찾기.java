1a15a0b808123fcf20fc706d5f726dfee4c70b8f
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2023-02-28 19:33:27 +0900

    [Silver IV] Title: 수 찾기, Time: 1280 ms, Memory: 53216 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -18,8 +18,6 @@ public class Main {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr);
-		
 		int inputArr[] = new int[Integer.parseInt(br.readLine())];
 		st = new StringTokenizer(br.readLine());
 		
@@ -27,24 +25,17 @@ public class Main {
 			inputArr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		Arrays.sort(arr);
+		
 		for (int i = 0; i < inputArr.length; i++) {
-			int result = binarySearch(arr, 0, arr.length-1, inputArr[i]);
-			if (result == -1) {
-				System.out.println(0);
-			} else {
+			int result = Arrays.binarySearch(arr, inputArr[i]);
+			if (result>= 0) {
 				System.out.println(1);
+			} else {
+				System.out.println(0);
 			}
 		}
 		
-	}
-	
-	public static int binarySearch(int[] arr, int start, int end, int key) {
-		if (start > end) return -1;
-		int mid = (start + end) / 2;
-		
-		if (arr[mid] == key) return mid;
-		else if (arr[mid] > key) return binarySearch(arr, start, mid-1, key);
-		else return binarySearch(arr, mid+1, end, key);
 	}
 
 }
