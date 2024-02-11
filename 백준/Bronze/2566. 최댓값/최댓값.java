08db7c2f9a74c1079fa1dd6dd2a7212eb710b933
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-02-12 02:33:23 +0900

    [Bronze III] Title: 최댓값, Time: 136 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int maxValue = arr[0][0];
+        int maxX = 0;
+        int maxY = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > maxValue) {
+                    maxValue = arr[i][j];
+                    maxX = i;
+                    maxY = j;
+                }
+            }
+        }
+
+        System.out.printf("%s%n%s %s", maxValue, maxX+1, maxY+1);
+    }
+}
