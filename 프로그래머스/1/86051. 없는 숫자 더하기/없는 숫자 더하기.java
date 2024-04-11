0f4bf1409622ba547a289e4fb9c33ab2ba5ee011
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-04-11 22:20:30 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int check[] = new int[10];
+        
+        for (int i=0; i<numbers.length; i++) {
+            check[numbers[i]]++;
+        }
+        
+        for (int i=0; i<check.length; i++) {
+            if (check[i] == 0) {
+                answer += i;
+            }
+        }
+                                        
+        return answer;
+    }
+}
