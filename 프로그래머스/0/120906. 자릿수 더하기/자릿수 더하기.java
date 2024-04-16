cabe93dffc7d0a65b9fc402ef2a56e382e33dc5d
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-04-17 00:07:10 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String numString = String.valueOf(n);
+        for (int i=0; i<numString.length(); i++) {
+            answer += numString.charAt(i) - '0';
+        }
+        
+        return answer;
+    }
+}
