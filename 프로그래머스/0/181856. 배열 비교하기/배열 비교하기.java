1599d3e076c00d5b1c12da90e4e26ca013b4c7d4
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-04-17 00:26:25 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        
+        if (arr1.length != arr2.length) {
+            answer = arr1.length > arr2.length? 1 : -1;
+            return answer;
+        }
+        
+        int sumOne = 0;
+        int sumTwo = 0;
+        for (int i=0; i<arr1.length; i++) {
+            sumOne += arr1[i];
+            sumTwo += arr2[i];
+        }
+        if (sumOne == sumTwo) {
+            answer = 0;
+        } else {
+            answer = sumOne > sumTwo? 1 : -1;
+        }
+        
+        return answer;
+    }
+}
