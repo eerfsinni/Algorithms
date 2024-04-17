5443489170d0238bead010b089a00a60d48b08c8
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-04-17 14:49:44 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        
+        for (int i=idx; i<arr.length; i++) {
+            if (arr[i] == 1) {
+                answer = i;
+                return answer;
+            }
+        }
+        
+        answer = -1;
+        return answer;
+    }
+}
