0520fe265a981c2fcd0abdb89c2d98e628222c7b
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-04-20 00:18:15 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.10 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        
+        while (chicken/10 != 0) {
+            chicken -= 9;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
