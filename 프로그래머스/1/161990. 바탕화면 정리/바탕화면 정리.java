ffb131ed0ee63ae53f95a6699b2a76ee43e904a9
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-07-01 22:39:13 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+        int rux = Integer.MIN_VALUE;
+        int ruy = Integer.MIN_VALUE;
+        
+        for (int i=0; i<wallpaper.length; i++) {
+            for (int j=0; j<wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rux = Math.max(rux, i);
+                    ruy = Math.max(ruy, j);
+                }
+            }
+        }
+                
+        int[] answer = new int[] {lux, luy, rux+1, ruy+1};
+        return answer;
+    }
+}
