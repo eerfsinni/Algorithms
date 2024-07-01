573bf981c9014076695e18838379caab4fcc4a72
eerfsinni <122072825+eerfsinni@users.noreply.github.com>
2024-07-01 22:16:16 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        for (int i=0; i<number.length; i++) {
+            for (int j=i+1; j<number.length; j++) {
+                for (int k=j+1; k<number.length; k++) {
+                    if (i == j || j == k) {
+                        continue;
+                    } else {
+                        if (number[i] + number[j] + number[k] == 0) {
+                            answer++;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
